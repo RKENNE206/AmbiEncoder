d1a58219217018f08b633fba7103461970309ce2
RKENNE206 <richikennedy@yahoo.co.uk>
2017-02-17 15:24:40 +0000

    second commit

--- AmbiEncoder/Source/PluginProcessor.cpp
+++ AmbiEncoder/Source/PluginProcessor.cpp
@@ -121,30 +121,38 @@ bool AmbiEncoderAudioProcessor::isBusesLayoutSupported (const BusesLayout& layou
 }
 #endif
 
-void AmbiEncoderAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+void AmbiEncoderAudioProcessor::processBlock(AudioSampleBuffer& buffer, MidiBuffer&
+	midiMessages)
 {
-    const int totalNumInputChannels  = getTotalNumInputChannels();
-    const int totalNumOutputChannels = getTotalNumOutputChannels();
+	const int totalNumInputChannels = getTotalNumInputChannels();
+	const int totalNumOutputChannels = getTotalNumOutputChannels();
+	const int numSamples = buffer.getNumSamples();
+	// In case we have more outputs than inputs, this code clears any output
+	// channels that didn't contain input data, (because these aren't
+	// guaranteed to be empty - they may contain garbage).
+	// This is here to avoid people getting screaming feedback
+	// when they first compile a plugin, but obviously you don't need to keep
+	// this code if your algorithm always overwrites all the output channels.
+	for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+		buffer.clear(i, 0, buffer.getNumSamples());
+	// Convert pan position in degrees to radians
+	float azimuth = (M_PI / 180.f) * panPosition;
+	// Get a pointer to each of the Ambisonic channels
+	float* channelDataW = buffer.getWritePointer(0);
+	float* channelDataX = buffer.getWritePointer(1);
+	float* channelDataY = buffer.getWritePointer(2);
+	float* channelDataZ = buffer.getWritePointer(3); // Not used
+													 // Loop through each sample
+	for (int i = 0; i < numSamples; i++)
+	{
+		float audioIn = channelDataW[i]; // Store input in temp variable
+										 // Do the encoding (horizontal only)
+		channelDataW[i] = audioIn * 0.707;
+		channelDataX[i] = audioIn * cos(azimuth);
+		channelDataY[i] = audioIn * sin(azimuth);
+	}
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        float* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
 }
-
 //==============================================================================
 bool AmbiEncoderAudioProcessor::hasEditor() const
 {
